0ce81a32b0f3b5980320543740d5fc0fdda8e2eb
Acla Putra <aclalead@gmail.com>
2021-05-23 01:45:59 +0700

    added constant folder

---
+++ 07_constant/constant.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstName string = "Acla"
+		lastName = "Putra"
+		value = 1000
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
