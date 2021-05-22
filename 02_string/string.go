c58b31bc87e6260b5770c3865d7e901622aa1b0b
Acla Putra <aclalead@gmail.com>
2021-05-23 01:40:30 +0700

    added string folder

---
+++ 02_string/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Acla")
+	fmt.Println("Acla Putra")
+	fmt.Println("M Acla Alamsyah Putra")
+
+	fmt.Println(len("Acla"))
+	fmt.Println("Acla Putra"[0])
+	fmt.Println("M Acla ALamsyah Putra"[1])
+}
